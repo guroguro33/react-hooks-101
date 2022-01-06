b38769ba9ddfb6d6f8b14ebe939d599c4c2d41e5
Toru <t02f447f@gmail.com>
2022-01-07 05:05:35 +0900

    create reducer for events

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -12,7 +12,7 @@ const events = (state = [], action) => {
       const event = {title: action.title, body: action.body};
       const length = state.length;
       const id = length === 0 ? 1 : state[length - 1].id + 1;
-      return state;
+      return [...state, {id, ...event}]; // id:idの場合、idでまとめられる
     case 'DELETE_EVENT':
       return state;
     case 'DELETE_ALL_EVENTS':
